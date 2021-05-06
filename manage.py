fa23cb162b23b091676c97ef302774806337adaa
Minh Thai Pham <minhthai.pham@foody.vn>
2021-05-06 15:56:01 +0700

    add cloud logging

--- manage.py
+++ manage.py
@@ -3,28 +3,45 @@
 import os
 import sys
 
-import logging
 from google.auth.exceptions import DefaultCredentialsError
 
 
-logger = logging.getLogger(__name__)
+def _setup_cloud_logging():
+    """
+    See https://cloud.google.com/logging/docs/setup/python#using_the_python_root_logger
+    """
+
+    # Imports the Cloud Logging client library
+    import google.cloud.logging
+
+    try:
+        # Instantiates a client
+        client = google.cloud.logging.Client()
+
+        # Retrieves a Cloud Logging handler based on the environment
+        # you're running in and integrates the handler with the
+        # Python logging module. By default this captures all logs
+        # at INFO level and higher
+        client.get_default_handler()
+        client.setup_logging()
+    except DefaultCredentialsError as error:
+        print(str(error))
 
 
-def main():
-    # for GCP Cloud Debugger
+def _setup_cloud_debugger():
     try:
         import googleclouddebugger
         googleclouddebugger.enable(
             breakpoint_enable_canary=True
         )
-    except ImportError:
-        pass
     except DefaultCredentialsError as error:
         logger.error(str(error))
 
+
+def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homie.settings.local')
-    logger.info(f"DJANGO_SETTINGS_MODULE={os.environ['DJANGO_SETTINGS_MODULE']}")
+    logger.warning(f"DJANGO_SETTINGS_MODULE={os.environ['DJANGO_SETTINGS_MODULE']}")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -37,4 +54,9 @@ def main():
 
 
 if __name__ == '__main__':
+    _setup_cloud_logging()
+    import logging
+    logger = logging.getLogger(__name__)
+
+    _setup_cloud_debugger()
     main()
