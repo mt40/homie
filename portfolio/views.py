ff6c69dc847be47a43ffa688edf3bba413c5e8b2
Minh Thai Pham <minhthai.pham@foody.vn>
2021-05-13 16:43:32 +0700

    add a log

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,16 @@
+import logging
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
+logger = logging.getLogger(__name__)
+
+
 def index(request):
-    return render(request, 'portfolio/index.html')
+    logger.info("hello world!")
+    return HttpResponse('ok')
+    # return render(request, 'portfolio/index.html')
 
 
 from django.views.generic import TemplateView
