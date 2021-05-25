695a13366385ac68dc95a44e688b58b897f6b6df
Minh Thai Pham <minhthai.pham@foody.vn>
2021-05-25 09:48:34 +0700

    add calculator link to admin index

--- admin_site/const.py
+++ admin_site/const.py
@@ -5,5 +5,6 @@ from django.db import models
 _admin_name = 'homie_admin'
 
 class UrlName(models.TextChoices):
+    ADMIN_INDEX = f"{_admin_name}:index"
     CALCULATOR = f'{_admin_name}:calculator'
     CALCULATOR_RESULT = f'{_admin_name}:calculator_result'
