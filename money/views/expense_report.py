a74d37a89265b037b5db551755b10a7e54cea045
Minh Thai Pham <minhthai.pham@foody.vn>
2021-06-14 21:32:47 +0700

    fix past expense values

--- money/views/expense_report.py
+++ money/views/expense_report.py
@@ -12,66 +12,92 @@ from money.models import Budget, Expense
 class PreviousExpenseInfo(pydantic.BaseModel):
     name: str
     value: NonNegativeInt
-    change_percent: int
+    # change compared to the first time range
+    value_change_percent: int
+
+    daily_avg: NonNegativeInt
+    daily_avg_change_percent: int
 
 
-def _get_previous_expenses(
+def _get_expense_info_in(
     time_ranges: List[Tuple[datetime.date, datetime.date]],
-    current_expense: int
 ) -> List[PreviousExpenseInfo]:
+    """
+    Get expense info in each time range.
+    """
+    first_value = 0
+    first_avg = 0
+
     rs = []
-    for from_date, to_date in time_ranges:
+    for i, (from_date, to_date) in enumerate(time_ranges):
         value = Expense.get_expense_value_in(from_date, to_date)
-        change_percent = int(100 - value / current_expense) if current_expense != 0 else 0
+        value_change_percent = (
+            int((first_value - value) / value * 100)
+            if value != 0 and i > 0
+            else 0
+        )
+
+        average = (
+            value / (to_date - from_date).days
+            if to_date != from_date
+            else value
+        )
+        avg_change_percent = (
+            int((first_avg - average) / average * 100)
+            if average != 0 and i > 0
+            else 0
+        )
+
         info = PreviousExpenseInfo(
             name=from_date.strftime("%b"),
-            value=Expense.get_expense_value_in(from_date, to_date),
-            change_percent=change_percent
+            value=value,
+            value_change_percent=value_change_percent,
+            daily_avg=average,
+            daily_avg_change_percent=avg_change_percent,
         )
+
         rs.append(info)
+
+        if i == 0:
+            first_value = value
+            first_avg = average
+
     return rs
 
 
+# testme
 class ExpenseReportView(TemplateView):
     template_name = "money/expense_report/index.html"
 
     def get_context_data(self, **kwargs):
-        from_date = datetime_util.first_date_current_month()
-        to_date = datetime_util.today()
+        today = datetime_util.today()
+        this_year = today.year
+        this_month = today.month
 
-        total_expense_value = Expense.get_expense_value_in(
-            from_date,
-            to_date,
-        )
-
-        this_year = from_date.year
-        this_month = from_date.month
-        previous_expenses = _get_previous_expenses(
+        expenses_by_time_range = _get_expense_info_in(
             [
                 (
+                    datetime_util.first_date_of(this_year, this_month),
+                    datetime_util.today(),
+                ),
+                (
                     datetime_util.first_date_of(this_year, this_month - 1),
-                    datetime_util.same_date_in(this_year, this_month - 1, to_date)
+                    datetime_util.same_date_in(this_year, this_month - 1, today)
                 ),
                 (
                     datetime_util.first_date_of(this_year, this_month - 2),
-                    datetime_util.same_date_in(this_year, this_month - 2, to_date)
+                    datetime_util.same_date_in(this_year, this_month - 2, today)
                 ),
             ],
-            total_expense_value
         )
-
-        days = (to_date - from_date).days
-        avg_expense_value = int(total_expense_value / days)
+        past_expenses = expenses_by_time_range[1:]
 
         return {
             **super().get_context_data(**kwargs),
-            'total_expense': {
-                'current': total_expense_value,
-                'previous': [
-                    ex.dict()
-                    for ex in previous_expenses
-                ]
-            },
-            'avg_expense_value': avg_expense_value,
+            'current_total': expenses_by_time_range[0].value,
+            'current_avg': expenses_by_time_range[0].daily_avg,
+            'past_expense_info': [
+                ex.dict() for ex in past_expenses
+            ],
             'budgets': Budget.objects.all()
         }
