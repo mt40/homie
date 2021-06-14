bb1d603b3f32347ec0c243b527d70baa2f71574e
mt40 <minhthai40@gmail.com>
2021-06-14 21:06:05 +0700

    add test

--- money/tests/test_expense_report_view.py
+++ money/tests/test_expense_report_view.py
@@ -1,6 +1,29 @@
+from django.urls import reverse
+
 from common.tests import BaseTestCase
+
+
+_expense_report_url = reverse("homie_admin:expense_report")
 
 
 class ExpenseReportViewTests(BaseTestCase):
     def test_expense_report_view_get(self):
-        pass  # todo
+        self.client.get(_expense_report_url)
+
+    def test_expense_report_view_context(self):
+        expense_this_month = ...
+        expense_change_percent = ...
+        avg_daily_expense = ...
+        avg_daily_change_percent = ...
+        today_expense = ...
+        budget_names = ...
+
+        res = self.client.get(_expense_report_url)
+
+        self.assertContains(res, expense_this_month)
+        self.assertContains(res, expense_change_percent)
+        self.assertContains(res, avg_daily_expense)
+        self.assertContains(res, avg_daily_change_percent)
+        self.assertContains(res, today_expense)
+        for name in budget_names:
+            self.assertContains(res, name)
